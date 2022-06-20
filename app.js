719645e893df541c63d2345f8e4d629fa2cf4c01
Amr Fahmy <amr.fahmy@linkdev.com>
2022-06-20 09:50:27 +0200

    UPD: post route

--- app.js
+++ app.js
@@ -8,25 +8,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
+  console.log('req.quary:', req.quary);
+  return handleSendMail();
+});
+
+app.get('/', (req, res) => {
   console.log('req.body:', req.body);
-
-  mailHelper.getSMTPsettings(function (smtpData) {
-    if (!smtpData) {
-      return res.status(500).send("Please configure your SMTP settings first");
-    }
-
-    mailHelper.setSmtpTransport(smtpData);
-    var data = { to: 'amr.fahmy@linkdev.com', message: 'Hello Dear', subject: 'Thank you for using exchange online service' };
-
-    mailHelper.setMaileTemplate('mail', data, function (html) {
-      data.html = html;
-      mailHelper.send(data, smtpData, function (error) {
-        console.log('send error:', error);
-        
-        return res.status(200).send('Ok');
-      });
-    });
-  });
+  return handleSendMail(req.body.to);
 });
 
 app.get('/health', (req, res) => {
@@ -37,3 +25,24 @@ app.get('/health', (req, res) => {
 app.listen(8080, () => {
   console.log('App listening on port 8080!');
 });
+
+function handleSendMail(mailAddress) {
+  let data = { to: mailAddress || 'amr.fahmy@linkdev.com', message: 'Hello Dear', subject: 'Thank you for using exchange online service' };
+
+  mailHelper.getSMTPsettings(function (smtpData) {
+    if (!smtpData) {
+      return res.status(500).send("Please configure your SMTP settings first");
+    }
+
+    mailHelper.setSmtpTransport(smtpData);
+
+    mailHelper.setMaileTemplate('mail', data, function (html) {
+      data.html = html;
+      mailHelper.send(data, smtpData, function (error) {
+        console.log('send error:', error);
+
+        return res.status(200).send('Ok');
+      });
+    });
+  });
+}
