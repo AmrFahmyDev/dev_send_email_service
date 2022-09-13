2062d5f7b95a1ad7beaaf6ec5d7de6f9e5485c8a
Amr Fahmy <amr.fahmy@linkdev.com>
2022-09-13 18:35:13 +0200

    UPD: using Kafka

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { Kafka } = require('kafkajs');
 const mailHelper = require('./helpers/mail_helper');
 const smtpSettingsTimeout = 60 * 60 * 24 * 30;
 
@@ -7,14 +8,59 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const kafka = new Kafka({
+  brokers: ['bank-services-cluster-kafka-bootstrap.bank-services.svc:9092'],
+  retry: {
+    initialRetryTime: 100,
+    retries: 8
+  }
+});
+const producer = kafka.producer()
+const consumer = kafka.consumer({ groupId: 'group1' })
+
+const run = async () => {
+  // Producing
+  await producer.connect()
+
+
+  // Consuming
+  await consumer.connect()
+  await consumer.subscribe({ topic: 'send-email', fromBeginning: true })
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      var mailAddress = message.mailAddress.toString();
+      console.log({
+        partition,
+        offset: message.offset,
+        value: mailAddress,
+      })
+
+      handleSendMail(req.body.mailAddress, (response) => {
+        producer.send({
+          topic: 'send-email',
+          messages: [
+            { value: response.msg },
+          ],
+        });
+        // return res.status(response.status).send(response.msg);
+      });
+    },
+  })
+}
+
+run().catch(console.error);
+
 app.get('/', (req, res) => {
   console.log('req.quary:', req.quary);
-  return handleSendMail(res,null);
+  return handleSendMail(res, null);
 });
 
 app.post('/', (req, res) => {
   console.log('req.body:', req.body);
-  return handleSendMail(res, req.body.mailAddress);
+  handleSendMail(req.body.mailAddress, (response) => {
+    return res.status(response.status).send(response.msg);
+  });
 });
 
 app.get('/health', (req, res) => {
@@ -26,12 +72,12 @@ app.listen(8080, () => {
   console.log('App listening on port 8080!');
 });
 
-function handleSendMail(res, mailAddress) {
+function handleSendMail(mailAddress, callback) {
   let data = { to: mailAddress || 'amr.fahmy@linkdev.com', message: 'Hello Dear', subject: 'Thank you' };
 
   mailHelper.getSMTPsettings(function (smtpData) {
     if (!smtpData) {
-      return res.status(500).send("Please configure your SMTP settings first");
+      return callback({ status: 500, msg: 'Please configure your SMTP settings first' }); //res.status(500).send("Please configure your SMTP settings first");
     }
 
     mailHelper.setSmtpTransport(smtpData);
@@ -41,7 +87,7 @@ function handleSendMail(res, mailAddress) {
       mailHelper.send(data, smtpData, function (error) {
         console.log('send error:', error);
 
-        return res.status(200).send('Ok');
+        return callback({ status: 200, msg: 'ok' }); //res.status(200).send('Ok');
       });
     });
   });
